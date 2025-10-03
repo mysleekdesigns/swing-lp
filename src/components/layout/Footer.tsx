4cab6a55930e00322352cb7df50bf977971f11f6
mysleekdesigns <simonlacey20@gmail.com>
2025-10-03 16:12:41 -0400

    created privacy policy page

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
 
   const navigationLinks = [
     { name: 'About', href: '/about' },
-    { name: 'Privacy Policy', href: '/privacy' },
+    { name: 'Privacy Policy', href: '/privacy-policy' },
     { name: 'Terms & Conditions', href: '/terms-and-conditions' },
     { name: 'Contact', href: '/contact' },
   ];
